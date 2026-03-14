2d0d4dd8d711a780799dead1174f755f6e5344f5
Lara <ooh.la.lara@gmail.com>
2026-03-14 08:00:41 +0000

    button layout

--- src/components/Insights.tsx
+++ src/components/Insights.tsx
@@ -3509,17 +3509,17 @@ const tryNextPrompts = useMemo(() => {
                         </button>
                         <button
                           type="button"
-                          className="px-3 py-2 rounded-xl border border-black/10 bg-white text-sm font-medium text-[rgb(var(--color-text-primary))] hover:bg-black/[0.03] transition-all"
-                          onClick={() => handlePatternFeedback('unsure', p)}
-                        >
-                          Not sure
-                        </button>
-                        <button
-                          type="button"
                           className="px-3 py-2 rounded-xl border border-[rgb(var(--color-primary-light)/0.55)] bg-[rgb(var(--color-primary-light))] text-[rgb(var(--color-primary-dark))] text-sm font-medium hover:opacity-95 transition-all"
                           onClick={() => handlePatternFeedback('no', p)}
                         >
                           No
+                        </button>
+                        <button
+                          type="button"
+                          className="px-3 py-2 rounded-xl border border-black/10 bg-white text-sm font-medium text-[rgb(var(--color-text-primary))] hover:bg-black/[0.03] transition-all"
+                          onClick={() => handlePatternFeedback('unsure', p)}
+                        >
+                          Not sure
                         </button>
                       </div>
                     </div>
